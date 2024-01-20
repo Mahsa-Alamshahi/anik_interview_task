8fb71a0ff1c1cda1ddef1aedbb55b8ddc7902b71
Mahsa <alamshahi.mahsa@gmail.com>
2024-01-20 06:30:49 +0330

    Dependencies added.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
@@ -68,4 +70,39 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     implementation("androidx.compose.material3:material3:1.2.0-alpha11")
 
+
+
+    // Dependency Injection
+    implementation("com.google.dagger:hilt-android:2.45")
+    kapt("com.google.dagger:hilt-android-compiler:2.45")
+    implementation("androidx.hilt:hilt-work:1.1.0")
+    kapt("androidx.hilt:hilt-compiler:1.1.0")
+    implementation("androidx.work:work-runtime-ktx:2.8.1")
+    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
+
+
+
+    // Lifecycle
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0-beta01")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0-beta01")
+    implementation ("androidx.lifecycle:lifecycle-runtime-compose:2.7.0-beta01")
+    implementation("androidx.compose.runtime:runtime-livedata:1.5.4")
+
+
+    //Navigation
+    implementation("androidx.navigation:navigation-compose:2.7.5")
+
+
+    // Multidex
+    implementation("androidx.multidex:multidex:2.0.1")
+
+    // Glide
+    implementation("com.github.bumptech.glide:compose:1.0.0-alpha.1")
+
+    // Moshi
+    implementation("com.squareup.moshi:moshi:1.14.0")
+
+
+    implementation("com.google.accompanist:accompanist-permissions:0.33.2-alpha")
+    implementation("com.google.accompanist:accompanist-systemuicontroller:0.27.0")
 }
