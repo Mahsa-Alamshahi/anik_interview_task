76289d03a55a369026e0914f54416045db3a3336
Mahsa <alamshahi.mahsa@gmail.com>
2024-01-20 06:50:42 +0330

    Anik Application class added. Hilt Module added. Gradle Java Version changed to 17. (Bug fixd.)

---
+++ app/src/main/java/com/anik/interview_task/AnikApplication.kt
@@ -0,0 +1,8 @@
+package com.anik.interview_task
+
+import androidx.multidex.MultiDexApplication
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+class AnikApplication(): MultiDexApplication()
--- app/src/main/java/com/anik/interview_task/ui/MainActivity.kt
+++ app/src/main/java/com/anik/interview_task/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.anik.interview_task
+package com.anik.interview_task.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,9 +11,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.anik.interview_task.ui.theme.AnikInterviewTaskTheme
+import dagger.hilt.android.AndroidEntryPoint
 
 
-
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
